a7f8d8efbe547218351b3706ff8a4247501d1d42
zuppettab <zuppettab@gmail.com>
2025-08-17 23:14:25 +0200

    Create sports.js

---
+++ utils/sports.js
@@ -0,0 +1,116 @@
+// utils/sports.js
+const sports = [
+  // 🔹 Sport Olimpici principali
+  { value: 'athletics', label: 'Athletics' },
+  { value: 'archery', label: 'Archery' },
+  { value: 'badminton', label: 'Badminton' },
+  { value: 'baseball', label: 'Baseball' },
+  { value: 'basketball', label: 'Basketball' },
+  { value: 'boxing', label: 'Boxing' },
+  { value: 'canoeing', label: 'Canoeing' },
+  { value: 'cycling', label: 'Cycling' },
+  { value: 'diving', label: 'Diving' },
+  { value: 'equestrian', label: 'Equestrian' },
+  { value: 'fencing', label: 'Fencing' },
+  { value: 'field_hockey', label: 'Field Hockey' },
+  { value: 'football', label: 'Football (Soccer)' },
+  { value: 'golf', label: 'Golf' },
+  { value: 'gymnastics', label: 'Gymnastics' },
+  { value: 'handball', label: 'Handball' },
+  { value: 'judo', label: 'Judo' },
+  { value: 'karate', label: 'Karate' },
+  { value: 'rowing', label: 'Rowing' },
+  { value: 'rugby', label: 'Rugby' },
+  { value: 'sailing', label: 'Sailing' },
+  { value: 'shooting', label: 'Shooting' },
+  { value: 'skateboarding', label: 'Skateboarding' },
+  { value: 'softball', label: 'Softball' },
+  { value: 'sport_climbing', label: 'Sport Climbing' },
+  { value: 'surfing', label: 'Surfing' },
+  { value: 'swimming', label: 'Swimming' },
+  { value: 'table_tennis', label: 'Table Tennis' },
+  { value: 'taekwondo', label: 'Taekwondo' },
+  { value: 'tennis', label: 'Tennis' },
+  { value: 'triathlon', label: 'Triathlon' },
+  { value: 'volleyball', label: 'Volleyball' },
+  { value: 'water_polo', label: 'Water Polo' },
+  { value: 'weightlifting', label: 'Weightlifting' },
+  { value: 'wrestling', label: 'Wrestling' },
+
+  // 🔹 Sport Invernali
+  { value: 'alpine_skiing', label: 'Alpine Skiing' },
+  { value: 'biathlon', label: 'Biathlon' },
+  { value: 'bobsleigh', label: 'Bobsleigh' },
+  { value: 'cross_country_skiing', label: 'Cross-Country Skiing' },
+  { value: 'curling', label: 'Curling' },
+  { value: 'figure_skating', label: 'Figure Skating' },
+  { value: 'freestyle_skiing', label: 'Freestyle Skiing' },
+  { value: 'ice_hockey', label: 'Ice Hockey' },
+  { value: 'luge', label: 'Luge' },
+  { value: 'nordic_combined', label: 'Nordic Combined' },
+  { value: 'short_track', label: 'Short Track Speed Skating' },
+  { value: 'skeleton', label: 'Skeleton' },
+  { value: 'ski_jumping', label: 'Ski Jumping' },
+  { value: 'snowboarding', label: 'Snowboarding' },
+  { value: 'speed_skating', label: 'Speed Skating' },
+
+  // 🔹 Sport da Combattimento & Arti Marziali
+  { value: 'aikido', label: 'Aikido' },
+  { value: 'brazilian_jiu_jitsu', label: 'Brazilian Jiu-Jitsu' },
+  { value: 'capoeira', label: 'Capoeira' },
+  { value: 'kung_fu', label: 'Kung Fu' },
+  { value: 'mixed_martial_arts', label: 'Mixed Martial Arts (MMA)' },
+  { value: 'muay_thai', label: 'Muay Thai' },
+  { value: 'sambo', label: 'Sambo' },
+  { value: 'sumo', label: 'Sumo' },
+
+  // 🔹 Sport Acquatici
+  { value: 'canoe_polo', label: 'Canoe Polo' },
+  { value: 'kayaking', label: 'Kayaking' },
+  { value: 'kitesurfing', label: 'Kitesurfing' },
+  { value: 'open_water_swimming', label: 'Open Water Swimming' },
+  { value: 'scuba_diving', label: 'Scuba Diving' },
+  { value: 'synchronized_swimming', label: 'Synchronized Swimming' },
+  { value: 'windsurfing', label: 'Windsurfing' },
+
+  // 🔹 Sport con Racchetta
+  { value: 'paddle', label: 'Paddle Tennis' },
+  { value: 'pickleball', label: 'Pickleball' },
+  { value: 'squash', label: 'Squash' },
+
+  // 🔹 Sport Motoristici
+  { value: 'formula1', label: 'Formula 1' },
+  { value: 'moto_gp', label: 'MotoGP' },
+  { value: 'rally', label: 'Rally' },
+  { value: 'karting', label: 'Karting' },
+  { value: 'motocross', label: 'Motocross' },
+
+  // 🔹 Sport di Precisione
+  { value: 'billiards', label: 'Billiards' },
+  { value: 'bowling', label: 'Bowling' },
+  { value: 'chess', label: 'Chess' },
+  { value: 'darts', label: 'Darts' },
+
+  // 🔹 Sport Outdoor & Estremi
+  { value: 'climbing', label: 'Climbing' },
+  { value: 'mountaineering', label: 'Mountaineering' },
+  { value: 'orienteering', label: 'Orienteering' },
+  { value: 'paragliding', label: 'Paragliding' },
+  { value: 'parkour', label: 'Parkour' },
+  { value: 'skating', label: 'Skating' },
+  { value: 'ultimate_frisbee', label: 'Ultimate Frisbee' },
+
+  // 🔹 Altri sport popolari
+  { value: 'american_football', label: 'American Football' },
+  { value: 'australian_rules', label: 'Australian Rules Football' },
+  { value: 'cricket', label: 'Cricket' },
+  { value: 'gaelic_football', label: 'Gaelic Football' },
+  { value: 'lacrosse', label: 'Lacrosse' },
+  { value: 'netball', label: 'Netball' },
+  { value: 'polo', label: 'Polo' },
+  { value: 'roller_hockey', label: 'Roller Hockey' },
+  { value: 'sepaktakraw', label: 'Sepak Takraw' },
+  { value: 'water_skiing', label: 'Water Skiing' }
+];
+
+export default sports;
